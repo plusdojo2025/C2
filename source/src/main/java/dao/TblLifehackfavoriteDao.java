09da771f9dfc5ed3c7e1a7319ecfbebecd526bea
plusdojo2025-Kawamoto <kawamoto-riku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 11:57:24 +0900

    DAO追加

--- source/src/main/java/dao/TblLifehackfavoriteDao.java
+++ source/src/main/java/dao/TblLifehackfavoriteDao.java
@@ -1,7 +1,68 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import dto.TblLifehackfavoriteDto;
 
 public class TblLifehackfavoriteDao extends CustomTemplateDao<TblLifehackfavoriteDto> {
+	
+	@Override
+	public List<TblLifehackfavoriteDto> select(TblLifehackfavoriteDto dto) {
+		Connection conn = null;
+		List<TblLifehackfavoriteDto> favorite = new ArrayList<>();
 
+		try {
+		conn = conn();
+
+			// SQL文を準備する
+			String sql = "SELECT * FROM tbl_lifehackfavorite WHERE familyId = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setString(1, dto.getFamilyId());
+			
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				TblLifehackfavoriteDto f = new TblLifehackfavoriteDto(rs.getString("familyId"), 
+								rs.getInt("lifehackNumber")
+				);				
+				favorite.add(f);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			favorite= null;
+		} finally {
+			// データベースを切断
+			close(conn);
+		}
+
+		// 結果を返す
+		return favorite;
+	}
+
+	@Override
+	public boolean insert(TblLifehackfavoriteDto dto) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean update(TblLifehackfavoriteDto dto) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean delete(TblLifehackfavoriteDto dto) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
 }
--- source/src/main/java/dao/TblSafestampDao.java
+++ source/src/main/java/dao/TblSafestampDao.java
@@ -1,7 +1,70 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import dto.TblSafestampDto;
 
 public class TblSafestampDao extends CustomTemplateDao<TblSafestampDto> {
 
+	@Override
+	public List<TblSafestampDto> select(TblSafestampDto dto) {
+		Connection conn = null;
+		List<TblSafestampDto> safestamp = new ArrayList<>();
+
+		try {
+		conn = conn();
+
+			// SQL文を準備する
+			String sql = "SELECT * FROM tbl_safestamp WHERE safeNumber = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, dto.getSafeNumber());
+			
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				TblSafestampDto g = new TblSafestampDto(rs.getInt("safeNumber"), 
+								rs.getString("status"),
+								rs.getString("familyId"), 
+								rs.getInt("userNumber")
+				);				
+				safestamp.add(g);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			safestamp = null;
+		} finally {
+			// データベースを切断
+			close(conn);
+		}
+
+		// 結果を返す
+		return safestamp;
+	}
+
+	@Override
+	public boolean insert(TblSafestampDto dto) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean update(TblSafestampDto dto) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+	@Override
+	public boolean delete(TblSafestampDto dto) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
 }

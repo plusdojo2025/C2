c3194b435619593444e7d069acc28373e0c6ee9a
plusdojo2025-maeda <maeda-kasumi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 10:33:59 +0900

    ログインサーブレット更新

--- source/src/main/java/servlet/CustomTemplateServlet.java
+++ source/src/main/java/servlet/CustomTemplateServlet.java
@@ -9,33 +9,87 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public abstract class CustomTemplateServlet extends HttpServlet {
+
 	
+
+	public boolean checkLogout(HttpServletRequest request,HttpServletResponse response) 
+
+			 throws IOException {
+
+		String logout = request.getParameter("logout");
+
+		boolean result = (logout != null);
+
+		if(result ) {
+
+			HttpSession session = request.getSession();
+
+			session.removeAttribute("id");
+
+			checkNoneLogin(request , response);
+
+		}
+
+		return result;
+
+	}
+
+	
+
 	protected final boolean checkNoneLogin(HttpServletRequest request, HttpServletResponse response) 
+
 			   throws IOException {
-		HttpSession session = request.getSession();
+
+		HttpSession session = request.getSession();		
+
 		boolean result = (session.getAttribute("id") == null);
+
 		if (result) {
+
 			// LOGINにリダイレクトする
+
 			response.sendRedirect("login");
+
 		}
+
 		return result;
+
 	}
+
 	
+
 	protected final boolean checkDoneLogin(HttpServletRequest request, HttpServletResponse response) 
+
 			   throws IOException {
+
 		HttpSession session = request.getSession();
+
 		boolean result = (session.getAttribute("id") != null);
+
 		if (result) {
+
 			// MENUにリダイレクトする
-			response.sendRedirect("menu");
+
+			response.sendRedirect("home");
+
 		}
+
 		return result;
+
 	}
+
 	
+
 	@Override
+
 	protected abstract void doGet(HttpServletRequest request, HttpServletResponse response)
+
 			throws ServletException, IOException;
+
 	@Override
+
 	protected abstract void doPost(HttpServletRequest request, HttpServletResponse response)
+
 			throws ServletException, IOException;
+
 }
--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -7,24 +7,101 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "LoginServlet", urlPatterns = { "/login" })
+@WebServlet("/login")
 public class LoginServlet extends CustomTemplateServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		if (checkDoneLogin(request,response)) {
+			return;
+		}
 		// ログインページにフォワードする
-		// jsp/login.jspにアクセスされてログインの画面が表示される
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		// jsp/Login.jspにアクセスされてログインの画面が表示される
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Login.jsp");
 		dispatcher.forward(request, response);
-
 	}
 
 	@Override
+	// ユーザーがmailとpwのリクエストを送信
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	}
+		if (checkDoneLogin(request,response)) {
+			return;
+		}
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("id", id);
+		
+		response.sendRedirect("home");
+		
+		
+		
+		// リクエストパラメーターを取得する
+		//request.setCharacterEncoding("UTF-8");
+		//String mail = request.getParameter("mail");
+		//String pw = request.getParameter("pw");
 
+		// ログイン処理を行う
+		// インスタンス化する
+		/*TblRegistuserDao iDao = new TblRegistuserDao();
+		
+		TblRegistuserDto ip = new TblRegistuserDto(mail, pw);
+		boolean ans = iDao.isLoginOK(ip);
+
+		if (ans) {
+			// if (iDao.isLoginOK(new IdPw(id, pw))) { // ログイン成功
+			// セッションスコープにIDを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("id", new LoginUser("mail"));
+
+			// メニューサーブレットにリダイレクトする
+			// 他のページをリクエストされたからフォワードではなくリダイレクト
+			response.sendRedirect("/webapp/MenuServlet");
+		} else { // ログイン失敗
+					// リクエストスコープに、メールアドレス、パスワード、戻り先を格納する
+			// request.setAttribute("result", new Result("ログイン失敗！", "IDまたはPWに間違いがあります。", "/webapp/LoginServlet"));
+
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Login.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		// 家族IDを新しく作成するリンク
+		/*
+		@WebServlet("/RegistFamily")
+		public class RegistFamily extends CustomTemplateServlet {
+			private static final long serialVersionUID = 1L;
+
+
+			// doGet
+			@Override
+			protected void doGet(HttpServletRequest request, HttpServletResponse response)
+					throws ServletException, IOException {
+				// jsp/RegistFamily.jspにフォワード（アクセスされて家族ID新規登録の画面が表示される）
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/RegistFamily.jsp");
+				dispatcher.forward(request, response);
+			}
+			// ユーザー情報を新しく作成するリンク
+			@WebServlet("/RegistUser")
+			public class RegistUser extends CustomTemplateServlet {
+				private static final long serialVersionUID = 1L;
+
+				// doGet
+				@Override
+				protected void doGet(HttpServletRequest request, HttpServletResponse response)
+						throws ServletException, IOException {
+					// jsp/RegistFamily.jspにフォワード（アクセスされて家族ID新規登録の画面が表示される）
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/RegistUser.jsp");
+					dispatcher.forward(request, response);
+*/
+			//	}
+			//}
+		//}
+	}
 }

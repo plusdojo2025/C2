0e197c80495ca6b15a71d7b0d0255991f1ebda71
plusdojo2025-Kawamoto <kawamoto-riku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 15:05:59 +0900

    LifeHackServlet全くできませんでした無念

--- source/src/main/java/servlet/LifeHackListServlet.java
+++ source/src/main/java/servlet/LifeHackListServlet.java
@@ -5,19 +5,18 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/LifeHackListServlet")
-public class LifeHackListServlet extends HttpServlet {
+public class LifeHackListServlet extends CustomTemplateServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// ライフハック登録ページにフォワードする
-		// jsp/LifeHackList.jspにアクセスされて防災バッグリストの画面が表示される
+		// jsp/LifeHackList.jspにアクセスされて防災ライフハックの画面が表示される
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/LifeHackList.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -25,5 +24,13 @@ public class LifeHackListServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		
+		
+		//ライフハックお気に入り登録
+		//成功時、jsp/LifeHackFavorite.jspにアクセスされる。
+		
 	}
 }

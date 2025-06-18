b949328c720484eb53d761877cc8694d34b36feb
plusdojo2025-maeda <maeda-kasumi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 15:25:44 +0900

    ライフハック申請更新2

--- source/src/main/java/servlet/LifeHackRequestServlet.java
+++ source/src/main/java/servlet/LifeHackRequestServlet.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.TblLifehackrequestDao;
-import dto.Result;
-import dto.TblLifehackrequestDto;
-
 //ライフハック申請サーブレットがURLであるLifeHackRequestと対応している
 @WebServlet("/LifeHackRequest")
 public class LifeHackRequestServlet extends HttpServlet {
@@ -29,7 +25,7 @@ public class LifeHackRequestServlet extends HttpServlet {
 	}
 
 	// post
-	@Override
+	/*@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
@@ -39,47 +35,22 @@ public class LifeHackRequestServlet extends HttpServlet {
 		String photo = request.getParameter("photo");
 		String textline = request.getParameter("textline");
 		
-		/*
-		// セッションからmailaddressを取得
-		// 40,41,44はHomeServletに沿って作成中のため、HomeServlet変更後こちらも変更が必要
-		HttpSession session = request.getSession();
-		String familyid = (String) session.getAttribute("family_id");
-		Integer usernumber = (Integer) session.getAttribute("user_number");
-
-		// セッション切れ防止
-		if (familyid == null || usernumber == null) {
-			response.sendRedirect("Login");
-			return;
-		}*/
-		
 		// データの処理を行うためのDTOのインスタンスを生成
-		TblLifehackrequestDto dto = new TblLifehackrequestDto();
+		TblLifehackrequestDto dto = new TblLifehackrequestDto(0, title, photo,textline);
 		dto.setRegistNumber(registnumber);
 		dto.setTitle(title);
 		dto.setPhoto(photo);	
 		dto.setTextline(textline);
 		
-
 		// データの処理を行うためのDAOのインスタンスを生成
 		TblLifehackrequestDao dao = new TblLifehackrequestDao();
 		boolean result = dao.insert(dto);
 		
 		// 申請処理を行う
-			if (dao.insert(new TblLifehackrequestDto(0, title, photo,textline))) { // 申請成功
-				request.setAttribute("result", new Result("complete", "申請完了しました。", "LifeHackRequest"));
+			if (dao.insert(dto)) { // 申請成功
+			request.setAttribute("result", new Result("complete", "申請完了しました。", "LifeHackRequest"));
 			} else { // 申請失敗
 				request.setAttribute("result", new Result("error", "未入力の項目があります。", "/WEB-INF/jsp/LifeHackRequest.jsp"));
-			}
-/*
-		// 申請完了したらLifeHackRequestServletにリダイレクト
-		if (result) {
-			System.out.println("申請完了しました。");
-			response.sendRedirect("LifeHackRequest");
-		} else {
-			// エラーを表示
-			System.out.println("未入力の項目があります。");
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/LifeHackRequest.jsp");
-			dispatcher.forward(request, response);
-			*/
+			
+			}*/
 		}
-	}

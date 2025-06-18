51d6a09f084715da25aa6456982dec01ff59d224
plusdojo2025-Tsukawaki <tsukawaki-sakura-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 12:59:47 +0900

    TblLifehacklistのテスト

--- source/src/main/java/servlet/LifeHackListServlet.java
+++ source/src/main/java/servlet/LifeHackListServlet.java
@@ -1,12 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.TblLifehacklistDao;
+import dto.TblLifehacklistDto;
 
 @WebServlet("/LifeHackListServlet")
 public class LifeHackListServlet extends CustomTemplateServlet {
@@ -16,6 +20,16 @@ public class LifeHackListServlet extends CustomTemplateServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// ライフハック登録ページにフォワードする
+		
+		//ライフハック一覧を表示する
+		// 検索処理を行う
+		TblLifehacklistDao lifeDao = new TblLifehacklistDao();
+		List<TblLifehacklistDto> lifeList = lifeDao.select(new TblLifehacklistDto(0, "", "", ""));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("lifeList", lifeList);
+
+		
 		// jsp/LifeHackList.jspにアクセスされて防災ライフハックの画面が表示される
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/LifeHackList.jsp");
 		dispatcher.forward(request, response);

63a5642f384896ee76ba5830e5496b4669a36580
plusdojo2025TamakawaOtoha <tamakawa-otoha-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-23 14:44:58 +0900

    ライフハック

--- source/src/main/java/servlet/LifeHackListServlet.java
+++ source/src/main/java/servlet/LifeHackListServlet.java
@@ -8,9 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import dao.TblLifehackfavoriteDao;
 import dao.TblLifehacklistDao;
+import dto.TblLifehackfavoriteDto;
 import dto.TblLifehacklistDto;
+
 
 @WebServlet("/LifeHackListServlet")
 public class LifeHackListServlet extends CustomTemplateServlet {
@@ -21,10 +25,14 @@ public class LifeHackListServlet extends CustomTemplateServlet {
 			throws ServletException, IOException {
 		// ライフハック登録ページにフォワードする
 		
+		// ── 検索キーワード取得 ──(キーワードがない場合空文字)
+		String keyword = request.getParameter("keyword");
+        if (keyword == null) keyword = "";
+        
 		//ライフハック一覧を表示する
 		// 検索処理を行う
 		TblLifehacklistDao lifeDao = new TblLifehacklistDao();
-		List<TblLifehacklistDto> lifeList = lifeDao.select(new TblLifehacklistDto(0, "", "", ""));
+		List<TblLifehacklistDto> lifeList = lifeDao.select(new TblLifehacklistDto(0, keyword, "", ""));
 
 		// 検索結果をリクエストスコープに格納する
 		request.setAttribute("lifeList", lifeList);
@@ -39,12 +47,41 @@ public class LifeHackListServlet extends CustomTemplateServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		
 		//リクエストパラメータの取得
 		request.setCharacterEncoding("UTF-8");
 		
+		String num2 = request.getParameter("lifehackNumber"); 
+		int lifehackNumber = Integer.parseInt(num2);
+		
 		
 		//ライフハックお気に入り登録
-		//成功時、jsp/LifeHackFavorite.jspにアクセスされる。
+		HttpSession session = request.getSession();
+		String FamilyId = (String) session.getAttribute("familyId");
 		
+		TblLifehackfavoriteDto dto = new TblLifehackfavoriteDto(0, FamilyId, lifehackNumber);
+		
+		// データの処理を行うためのDAOのインスタンスを生成
+		TblLifehackfavoriteDao dao = new TblLifehackfavoriteDao();
+		
+		
+		//成功時、jsp/LifeHackFavorite.jspにアクセスされる。
+	
+			if (dao.insert(dto)) {
+				//ライフハック一覧を表示する
+				// 検索処理を行う
+				TblLifehacklistDao lifeDao = new TblLifehacklistDao();
+				List<TblLifehacklistDto> lifeList = lifeDao.select(new TblLifehacklistDto(0, "", "", ""));
+
+				// 検索結果をリクエストスコープに格納する
+				request.setAttribute("lifeList", lifeList);
+
+				
+				// jsp/LifeHackList.jspにアクセスされて防災ライフハックの画面が表示される
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/LifeHackList.jsp");
+				dispatcher.forward(request, response);
+			}else {RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/LifeHackList.jsp");
+			dispatcher.forward(request, response);
+			}
 	}
 }

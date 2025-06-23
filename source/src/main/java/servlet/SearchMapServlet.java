6c25f504006667d58c2284dd788887aba9c68e67
plusdojo2025-maeda <maeda-kasumi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-23 10:48:35 +0900

    ログインセッション更新

--- source/src/main/java/servlet/SearchMapServlet.java
+++ source/src/main/java/servlet/SearchMapServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dto.IdPw;
+
 @WebServlet("/SearchMapServlet")
 public class SearchMapServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,27 +20,26 @@ public class SearchMapServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 	// セッションから family_id,user_numberを取得、LoginServletと統一の名前
-			HttpSession session = request.getSession(false);
-			// セッションから familyId を取得
-			if (session == null) {
-		        // 未ログイン時の処理（ログイン画面へリダイレクト）
-		        response.sendRedirect("login");
-		        return;
-		    }
+	// 必ずセッションを取得（なければ作成）
+			HttpSession session = request.getSession(true);
 			
-			String mailaddress = (String) session.getAttribute("mailaddress"); 
+			IdPw idpw = (IdPw) session.getAttribute("mail"); 
+			// idpw があればメールを取り出し、なければ null にする
+			String mail = (idpw != null) ? idpw.getMail() : null;
 			// セッション用のメールアドレスにメールアドレスが入っているか確認する
-			System.out.println("mailaddress:" + mailaddress);
+			System.out.println("mail:" + mail);
 			
-			if (mailaddress == null) {
+			if (mail == null) {
 		        // 未ログイン時の処理（ログイン画面へリダイレクト）
-		        response.sendRedirect("login");
+				// ログインしていないと判断した時の処理
+		        response.sendRedirect(request.getContextPath() + "/login");
 		        return;
-		    }
+		    } else {
 			// ハザードマップ検索ページにフォワードする
 			// jsp/SearchMap.jspにアクセスされてハザードマップ検索ページの画面が表示される
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/SearchMap.jsp");
 			dispatcher.forward(request, response);
+		  }
 	}
 	
 	@Override
